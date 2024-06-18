efc21285ca1a32f45cacac76ba96f69007758b9b
Azemroxy <156131040+Azemroxy@users.noreply.github.com>
2024-06-19 00:01:06 +0200

    Update manager.js

--- manager.js
+++ manager.js
@@ -1,9 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '6676236322:AAELJ7WzBma1rcpIpsHZp9lIBg8hmVE2M0o';
-
+const token = '6676236322:AAG1t77ijj_4qiEyUsYa49e7l7XB7WJCBng';
 
 const bot = new TelegramBot(token, { polling: true });
-
 
 bot.on('message', (msg) => {
   // Convert message text to lowercase to handle case insensitivity
@@ -26,12 +24,14 @@ bot.on('message', (msg) => {
     bot.sendMessage(chatId, "Here are the support tags: @oppresses, @HARDCORE369");
   }
 
-  if (text.includes("refund") {
+  // Respond to 'refund' keyword
+  if (text.includes("refund")) {  // Added the missing parenthesis here
     bot.sendMessage(chatId, "Please dm only one Staff Member at a time with your OrderID within 5 Minutes to get it checked. If your card is live, 1$ will be deducted from your Balance. If found dead, your balance will be refunded.");
   }
 
+  // Respond to 'help' or 'faq' keywords
   if (text.includes("help") || text.includes("faq")) {
-    bot.sendMessage(chatId, "Check out out FAQ Section on our Shop!");
+    bot.sendMessage(chatId, "Check out our FAQ Section on our Shop!");
   }
 });
 
