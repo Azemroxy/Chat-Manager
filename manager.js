0bf38ca5751c241e5f846fbc8839594882ec4721
Azemroxy <156131040+Azemroxy@users.noreply.github.com>
2024-06-19 21:34:29 +0200

    Update manager.js

--- manager.js
+++ manager.js
@@ -1,10 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '6676236322:AAG1t77ijj_4qiEyUsYa49e7l7XB7WJCBng';
+const fetch = require('node-fetch');
+const token = '6676236322:AAELJ7WzBma1rcpIpsHZp9lIBg8hmVE2M0o';
 
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
-  // Convert message text to lowercase to handle case insensitivity
   const chatId = msg.chat.id;
   const text = msg.text.toLowerCase();
 
@@ -16,22 +16,47 @@ bot.on('message', (msg) => {
 
   // Respond to 'spoof' or 'spoofing' keywords
   if (text.includes("spoof") || text.includes("spoofing")) {
-    bot.sendMessage(chatId, "Take a look at our Spoofing/OTP Section at our Shop");
+    bot.sendMessage(chatId, "Need a Spoof/OTP? Take a look at our Spoofing/OTP Section at our Shop");
   }
 
   // Respond to 'support' or 'staff' keywords
   if (text.includes("support") || text.includes("staff")) {
-    bot.sendMessage(chatId, "Here are the support tags: @oppresses, @HARDCORE369");
+    bot.sendMessage(chatId, "Support: @oppresses, @HARDCORE369, @trustedlizard");
   }
 
   // Respond to 'refund' keyword
-  if (text.includes("refund")) {  // Added the missing parenthesis here
+  if (text.includes("refund")) {
     bot.sendMessage(chatId, "Please dm only one Staff Member at a time with your OrderID within 5 Minutes to get it checked. If your card is live, 1$ will be deducted from your Balance. If found dead, your balance will be refunded.");
   }
 
   // Respond to 'help' or 'faq' keywords
   if (text.includes("help") || text.includes("faq")) {
     bot.sendMessage(chatId, "Check out our FAQ Section on our Shop!");
+  }
+
+  // BIN check command - expecting messages like "/bin 414720"
+  if (text.startsWith("/bin ")) {
+    const bin = text.split(" ")[1]; // get the BIN number from the command
+    if (bin.length === 6 && /^\d+$/.test(bin)) { // Check if BIN is a 6-digit number
+      const url = `https://data.handyapi.com/bin/${bin}`;
+
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          if (data.Status === "SUCCESS") {
+            // Format the reply message
+            const reply = `Scheme: ${data.Scheme}\nType: ${data.Type}\nTier: ${data.CardTier}\nIssuer: ${data.Issuer}\nCountry: ${data.Country.Name}`;
+            bot.sendMessage(chatId, reply);
+          } else {
+            bot.sendMessage(chatId, "No data available for the provided BIN.");
+          }
+        })
+        .catch(error => {
+          bot.sendMessage(chatId, "Failed to retrieve BIN information due to a network error or bad response. Please try again.");
+        });
+    } else {
+      bot.sendMessage(chatId, "Please enter a valid 6-digit BIN number.");
+    }
   }
 });
 
